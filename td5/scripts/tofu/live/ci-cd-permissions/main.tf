735d252d6581cac5e1ffc1d7bd880b14a17fe2a3
romain-af <romainfer@hotmail.com>
2025-02-02 19:26:03 +0100

    Add infrastructure tests workflow

--- td5/scripts/tofu/live/ci-cd-permissions/main.tf
+++ td5/scripts/tofu/live/ci-cd-permissions/main.tf
@@ -1,16 +1,16 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "eu-north-1"
 }
 
 module "oidc_provider" {
-  source = "github.com/brikis98/devops-book//ch5/tofu/modules/github-aws-oidc"
+  source = "github.com/romain-af/devops-base//td5/scripts/tofu/modules/github-aws-oidc"
 
   provider_url = "https://token.actions.githubusercontent.com" 
 
 }
 
 module "iam_roles" {
-  source = "github.com/brikis98/devops-book//ch5/tofu/modules/gh-actions-iam-roles"
+  source = "github.com/romain-af/devops-base//td5/scripts/tofu/modules/gh-actions-iam-roles"
 
   name              = "lambda-sample"                           
   oidc_provider_arn = module.oidc_provider.oidc_provider_arn    
@@ -18,13 +18,13 @@ module "iam_roles" {
   enable_iam_role_for_testing = true                            
 
   # TODO: fill in your own repo name here!
-  github_repo      = "brikis98/fundamentals-of-devops-examples" 
+  github_repo      = "romain-af/devops-base" 
   lambda_base_name = "lambda-sample"                            
 
   enable_iam_role_for_plan  = true                                
   enable_iam_role_for_apply = true                                
 
   # TODO: fill in your own bucket and table name here!
-  tofu_state_bucket         = "fundamentals-of-devops-tofu-state" 
-  tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state" 
+  tofu_state_bucket         = "tofu-state-romain" 
+  tofu_state_dynamodb_table = "tofu-state-lock" 
 }
