0752e3053840cf4cd381fb4719f53d1af2dc1eec
romain-af <romainfer@hotmail.com>
2025-02-02 17:51:31 +0100

    Add sample-app and workflow

--- td5/scripts/sample-app/app.js
+++ td5/scripts/sample-app/app.js
@@ -1,15 +1,14 @@
-// app.js
-const http = require('http');
+const express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 8080;
+const app = express();
+app.set('view engine', 'ejs');
 
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello, World!\n');
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-server.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/name/:name', (req, res) => {
+  res.render('hello', {name: req.params.name});
 });
+
+module.exports = app;
